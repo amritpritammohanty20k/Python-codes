68e678978cf577fe1060f3be2d48991d2c5ab6eb
amritpritammohanty20k <amritpritammohanty86@gmail.com>
2025-08-02 00:56:09 +0530

    updated P5.py - fixed logic

--- CH2_PS/P5.py
+++ CH2_PS/P5.py
@@ -2,5 +2,6 @@
 
 a=int(input("Enter Number 1: "))
 b=int(input("Enter Number 2: "))
-x=a+b
+x=a+b # if we want to skip this step then we put bracket around 
+#, a+b like this (a+b)thn devided by 2 =(a+b)/2
 print("Average of two Numbers : ",x/2)
