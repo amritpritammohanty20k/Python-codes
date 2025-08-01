156e9d16732170346b52b4ec9a58910a3e11ea85
amritpritammohanty20k <amritpritammohanty86@gmail.com>
2025-08-01 20:41:33 +0530

    Added P5 and P6 Python files

---
+++ CH2_PS/P5.py
@@ -0,0 +1,6 @@
+#Find average of two nos , Entered by the user by using  keyboard
+
+a=int(input("Enter Number 1: "))
+b=int(input("Enter Number 2: "))
+x=a+b
+print("Average of two Numbers : ",x/2)
---
+++ CH2_PS/P6.py
@@ -0,0 +1,5 @@
+#Calculate the square of the number .
+
+x=int(input("Enter Number 1: "))
+print("Square of the Number is: ",x*x)
+#square of no- [x*x] / [x**2]
