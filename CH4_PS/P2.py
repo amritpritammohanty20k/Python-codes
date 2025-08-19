43541f57fb342f91912f7b9b6d61e27f505edd37
AmritPritamMohanty20k <amritpritammohanty86@gmail.com>
2025-08-19 17:10:42 +0530

    added files P1.py and P2.py

---
+++ CH4_PS/P2.py
@@ -0,0 +1,18 @@
+#Write a program to accept marks of 6 students and display them in a sorted manner.
+mark=[]
+
+f1=input('Enter student mark: ')
+mark.append(f1)
+f2=input('Enter student mark: ')
+mark.append(f2)
+f3=input('Enter student mark: ')
+mark.append(f3)
+f4=input('Enter student mark: ')
+mark.append(f4)
+f5=input('Enter student mark: ')
+mark.append(f5)
+f6=input('Enter student mark: ')
+mark.append(f6) 
+f7=input('Enter student mark: ')
+mark.append(f7)
+print(mark)
