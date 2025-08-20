0f9ef852168f0c3a3cab53fe9dc28618e5eba1dc
AmritPritamMohanty20k <amritpritammohanty86@gmail.com>
2025-08-20 14:04:42 +0530

    added new folder CH5

---
+++ CH5/Dictionary.py
@@ -0,0 +1,25 @@
+# Dictionary is a list of key values 
+# Here 'Amrit','Omm','Krishna','Don' all are key values or simply key .
+marks = {
+    "Amrit":100,
+    "Omm":95,
+    "Krishna":101,
+    "Don":87,
+    "list":[1,4,9]
+}
+print(marks,type(marks))
+print(marks["Krishna"]) # Tells the corrensponding value of the key ! 
+"""
+                    PROPERTIES OF PYTHON DISCTIONARY
+
+                    #it is mutable
+                    #it is unordered
+                    #it is indexed
+                    #cannot contain duplicate keys
+
+
+
+
+
+
+"""
