dc95c4cb98726b134085bca465843fa19aa7cc2a
AmritPritamMohanty20k <amritpritammohanty86@gmail.com>
2025-08-20 12:13:10 +0530

    updated a file 6_Operation.py

---
+++ CH4_PS/P3.py
@@ -0,0 +1,11 @@
+#Check  that a tuple type cannot be changed in python. 
+a=(20,"amrit")
+a[1]="pritam"
+
+
+
+
+
+'''
+tuples are immutable they cannot be changed .
+'''
---
+++ CH4_PS/P4.py
@@ -0,0 +1,5 @@
+# Write a program to sum a list with 4 numbers.
+list1=['44','4.9',]
+list2=['12','9.6']
+combain=list1+list2
+print(combain)
