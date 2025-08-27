3345af931e9c226768fcc737e4f9299cd81fae08
AmritPritamMohanty20k <amritpritammohanty86@gmail.com>
2025-08-27 09:52:39 +0530

    updated 05_Set_Operation.py

---
+++ CH5_PS/P1.py
@@ -0,0 +1,19 @@
+# Write a program to create a dictionary of Hindi words with values as English tranlation . Provide user with an option to look it up !
+words={
+    "पुस्तक": "Book",
+
+"जल" : "Water",
+
+"आकाश" : "Sky",
+
+"खुशी": "Happiness",
+
+"सूरज": "Sun"
+}
+x=input("Enter the word that you need : ")
+print(words[x])
+
+
+# STEP 1 : Declareing a variable [ x ] then assigned the user input .
+# STEP 2 : Then print it .
+
